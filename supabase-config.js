ae9e510f34929b2394b90c9dc3a6a35b60001711
Csatangoló Lovarda <csatangolomedia@gmail.com>
2026-06-30 09:28:13 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// CSATANGOLÓ FÓRUM – TESZT SUPABASE KAPCSOLAT
+// Ide csak a TESZT Supabase adatok kerüljenek.
+// A service_role / secret kulcsot SOHA ne másold ide.
+
+window.CSATANGOLO_SUPABASE_URL = "https://brbtvjikquaanypccvlf.supabase.co";
+window.CSATANGOLO_SUPABASE_ANON_KEY = "IDE_MASOLD_A_TESZT_ANON_VAGY_PUBLISHABLE_KEYT";
