d163ed61693ad5af951d0db60b645dba81730c36
Csatangoló Lovarda <csatangolomedia@gmail.com>
2026-06-30 09:41:14 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,4 +3,4 @@
 // A service_role / secret kulcsot SOHA ne másold ide.
 
 window.CSATANGOLO_SUPABASE_URL = "https://brbtvjikquaanypccvlf.supabase.co";
-window.CSATANGOLO_SUPABASE_ANON_KEY = "IDE_MASOLD_A_TESZT_ANON_VAGY_PUBLISHABLE_KEYT";
+window.CSATANGOLO_SUPABASE_ANON_KEY = "sb_publishable_ROEEHu7pPuecGew1eJIYQQ_Ud5hcOhV";
